17b694747387d362e7060a25794f5cd98cba7032
Carlos Alcala <alcala21@gmail.com>
2020-08-16 19:50:19 -0500

    Solution 2 to stage 2. This solution is simpler.

--- calculator.py
+++ calculator.py
@@ -2,12 +2,9 @@
 while True:
     elems = input().split(" ")
     try:
-        if len(elems) == 2:
-            print(int(elems[0]) + int(elems[1]))
-        elif len(elems) == 1:
-            if elems[0] == '/exit':
-                print("Bye!")
-                break
-            print(int(elems[0]))
+        if elems[0] == '/exit':
+            print('Bye!')
+            break
+        print(sum([int(x) for x in elems]))
     except ValueError:
         pass
