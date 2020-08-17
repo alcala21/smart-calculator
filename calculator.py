5a2ee7a0af8a4ec4a34cdf63e24d26ed065ab851
Carlos Alcala <alcala21@gmail.com>
2020-08-17 14:52:18 -0500

    Solution to stage 3.

--- calculator.py
+++ calculator.py
@@ -1,10 +1,11 @@
-# write your code here
 while True:
     elems = input().split(" ")
     try:
         if elems[0] == '/exit':
             print('Bye!')
             break
+        elif elems[0] == '/help':
+            print("The program calculates the sum of numbers")
         print(sum([int(x) for x in elems]))
     except ValueError:
         pass
