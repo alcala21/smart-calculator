5ca3c397bd15a939c8c04c0d2ba81a8479becddf
Carlos Alcala <alcala21@gmail.com>
2020-08-20 01:02:32 -0500

    Stage 6 implemented.

--- calculator.py
+++ calculator.py
@@ -1,64 +1,140 @@
-def get_symbol(symbols):
-    if len(symbols) == 1:
-        return symbols[0]
-    if symbols[0] == symbols[1]:
-        symbol = '+'
-    else:
-        symbol = '-'
-    if len(symbols) == 2:
-        return symbol
-    new_symbols = symbol + symbols[2:]
-    return get_symbol(new_symbols)
+class BreakCommand(Exception):
+    pass
 
 
-def extract_character(x):
+class HelpCommand(Exception):
+    pass
+
+
+class UnknownCommand(Exception):
+    pass
+
+
+class InvalidExpression(Exception):
+    pass
+
+
+class UnknownVariable(Exception):
+    pass
+
+
+class InvalidIdentifier(Exception):
+    pass
+
+
+class InvalidAssignment(Exception):
+    pass
+
+
+def get_sign(signs):
+    if len(signs) == 1:
+        return signs
+    sign = "+" if signs[0] == signs[1] else "-"
+    if len(signs) == 2:
+        return sign
+    new_signs = sign + signs[2:]
+    return get_sign(new_signs)
+
+
+def extract_character(x, v_dict):
     try:
         number = int(x)
         return number
     except ValueError:
-        if any([y.isalpha() for y in x]):
-            raise ValueError
+        if x in v_dict:
+            return v_dict[x]
+        elif x.isalpha():
+            raise UnknownVariable
         elif not any([y not in '+-' for y in x]):
-            return get_symbol(x)
+            return get_sign(x)
+        else:
+            raise InvalidIdentifier
 
 
 def perform_operation(x_list):
+    if x_list[1] == "+":
+        return x_list[0] + x_list[2]
+    return x_list[0] - x_list[2]
+
+
+def prepare_variables(x_list):
     if len(x_list) == 3:
-        if x_list[1] == "+":
-            return x_list[0] + x_list[2]
-        elif x_list[1] == '-':
-            return x_list[0] - x_list[2]
+        if x_list[1] in "+-":
+            return perform_operation(x_list)
         else:
-            raise ValueError('You need operation symbols.')
+            raise InvalidExpression
     else:
-        raise ValueError('List needs three elements.')
+        raise InvalidExpression
 
 
 def calculate_list(x_list):
     try:
         if len(x_list) == 3:
-            return perform_operation(x_list)
+            return prepare_variables(x_list)
         elif len(x_list) == 1:
             return int(x_list[0])
-        p_val = perform_operation(x_list[:3])
+        p_val = prepare_variables(x_list[:3])
         new_list = [p_val] + x_list[3:]
         return calculate_list(new_list)
-    except TypeError:
-        raise ValueError
+    except Exception:
+        raise InvalidExpression
 
+
+def assign_value(x, v_dict):
+    x = x.replace(" ", "")
+    if x.count("=") > 1:
+        raise InvalidAssignment
+    x_list = x.split("=")
+    try:
+        if x_list[0].isalpha():
+            v_dict[x_list[0]] = int(x_list[-1])
+        else:
+            raise InvalidIdentifier
+    except ValueError:
+        if x_list[-1] in v_dict:
+            v_dict[x_list[0]] = v_dict[x_list[-1]]
+        elif not x_list[-1].isalpha():
+            raise InvalidAssignment
+        else:
+            raise UnknownVariable
+
+
+def check_command(x):
+    if x == '/exit':
+        raise BreakCommand
+    elif x == '/help':
+        raise HelpCommand
+    else:
+        raise UnknownCommand
+
+
+values = dict()
 
 while True:
-    elems = input().strip().split(" ")
+    elems = input().strip()
     try:
-        if elems[0] == '/exit':
-            print('Bye!')
-            break
-        elif elems[0] == '/help':
-            print("The program performs addition and subtraction of integers")
-        elif len(elems[0]) > 0 and elems[0][0] == '/':
-            print("Unknown command")
-        elif elems[0] != '':
-            chars = [extract_character(x) for x in elems]
+        if '=' in elems:
+            assign_value(elems, values)
+        elif elems.startswith('/'):
+            check_command(elems)
+        elif elems in values:
+            print(values[elems])
+        elif elems != '':
+            elems = elems.split(" ")
+            chars = [extract_character(x, values) for x in elems]
             print(calculate_list(chars))
-    except ValueError:
-        print("Invalid expression")
+    except BreakCommand:
+        print("Bye!")
+        break
+    except HelpCommand:
+        print("This program performs addition and subtraction of integer variables.")
+    except UnknownCommand:
+        print('Unknown command')
+    except InvalidExpression:
+        print('Invalid expression')
+    except UnknownVariable:
+        print('Unknown variable')
+    except InvalidIdentifier:
+        print('Invalid identifier')
+    except InvalidAssignment:
+        print('Invalid assignment')
